16d5a4ccb7c8f814b0563988cb3a1e8e5c2d8606
A.Srour <75044587+A-Srour@users.noreply.github.com>
2021-07-22 01:12:39 +0300

    First Commit

---
+++ src/main/java/srour/said/ul/TCPChat/client/MainClient.java
@@ -0,0 +1,8 @@
+package srour.said.ul.TCPChat.client;
+
+public class MainClient {
+
+	public static void main(String[] args) {
+		new Client();
+	}
+}
---
+++ src/main/java/srour/said/ul/TCPChat/server/MainServer.java
@@ -0,0 +1,8 @@
+package srour.said.ul.TCPChat.server;
+
+public class MainServer {
+
+	public static void main(String[] args) {
+		new Server();
+	}
+}
